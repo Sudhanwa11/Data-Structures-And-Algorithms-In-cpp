cab3e87360f70d648ca69d1aeda2b323e54e37b9
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-04-01 20:33:21 +0530

    Create findingelement.cpp

---
+++ ARRAYS CODES/Binary search/finding element/findingelement.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int Binarysearch (int arr[], int size, int element) {
+    int start = 0;
+    int end = size - 1;
+    int mid = start + (end-start)/2;
+    while (start<end) {
+        //finding mid point
+        if (arr[mid] == element) {
+            return mid;
+        }
+        //right path
+        if (element > arr[mid]) {
+            start = mid + 1;
+        }
+        //left path
+        else {
+           end = mid - 1; 
+        }
+        mid = start + (end-start)/2;
+    }
+    return -1;
+}
+int main() {
+   //example arrays
+   int even [6] = {2,4,6,8,12,18};
+   int odd [5] = {3,8,11,14,16};
+   int evenindex = Binarysearch (even, 6 , 6);
+   cout<< "index of 6 is " << evenindex <<endl;
+   int oddindex = Binarysearch (odd, 5, 14);
+   cout<< "index of 14 is " << oddindex <<endl;
+   return 0;
+}
