51756dd2448f454e5ff7893f27aad9cf9a7778a7
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-04-20 18:27:58 +0530

    Create length of string.cpp

---
+++ ARRAYS CODES/STRINGS/Lenght of string/length of string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int getlength (char name[]) {
+    int count = 0;
+    for (int i=0; name[i] != '\0'; i++) {
+        count++;
+    }
+    return count;
+}
+int main() {
+    char name[20];
+    cout<< "enter name "<<endl;
+    cin>>name;
+    cout<< "your name is ";
+    cout<< name<<endl;
+    cout<< "length "<< getlength(name)<<endl;
+    return 0;
+}
