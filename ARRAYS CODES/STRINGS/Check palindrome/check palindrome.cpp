6cdffc9d6f1040cb122db9c8e8c4ecfd3765f5ea
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-04-20 19:09:34 +0530

    Create check palindrome.cpp

---
+++ ARRAYS CODES/STRINGS/Check palindrome/check palindrome.cpp
@@ -0,0 +1,14 @@
+bool isPalindrome(string &s) {
+	int start = 0;
+	int end = s.size()-1;
+	while (start<=end) {
+		if (s[start] != s[end]) {
+			return 0;
+		}
+		else {
+			start++;
+			end--;
+		}
+	}
+  return 1;
+}
