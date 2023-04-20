e184700b20a3c7c0ac8d8d8fba62881da74e5feb
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-04-20 19:43:25 +0530

    Create check palindrome.cpp

---
+++ ARRAYS CODES/STRINGS/Check if a string is a palindrome/check palindrome.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+bool checkPalindrome(string s) {
+    transform(s.begin(), s.end(), s.begin(), ::tolower);
+    string str="";
+    for(int i=0;i<=s.size()-1;i++){
+        if(s.at(i)>='a'&&s.at(i)<='z'||s.at(i)>='0'&&s.at(i)<='9'){
+            str+=s.at(i);
+        }
+    }
+    string rev=str;
+    int start=0;
+    int end=rev.size()-1;
+    while(start<end){
+        swap(rev.at(start++),rev.at(end--));
+    }
+    return rev==str;
+}
