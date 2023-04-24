442f8882b5fe733bc750dc3d8799cb3827e93a15
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-04-24 20:44:02 +0530

    Update 2darray.cpp

--- ARRAYS CODES/2D ARRAYS/2D array/2darray.cpp
+++ ARRAYS CODES/2D ARRAYS/2D array/2darray.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include <climits>
 using namespace std;
-bool isPresent (int arr[][4], int target, int row, int col) {
+bool isPresent (int arr[][3], int target, int row, int col) {
     for (int row=0; row<3; row++) {
-        for (int col=0; col<4; col++) {
+        for (int col=0; col<3; col++) {
             if (arr[row][col] == target) {
                 return 1;
             }
@@ -10,13 +11,42 @@ bool isPresent (int arr[][4], int target, int row, int col) {
     }
     return 0;
 }
+//to print sum of rows
+void printSum (int arr[][3], int row, int col) {
+    cout<< "printing the sum-> "<<endl;
+    for (int row=0; row<3; row++) {
+        int sum = 0;
+        for (int col=0; col<3; col++) {
+            sum += arr[row][col];
+        }
+        cout << sum << " ";
+    }
+    cout<<endl;
+}
+int largestrowSum (int arr[][3], int row, int col) {
+    int maxi = INT_MIN;
+    int rowIndex = -1;
+    for (int row=0; row<3; row++) {
+        int sum = 0;
+        for (int col=0; col<3; col++) {
+            sum += arr[row][col];
+        }
+        if (sum > maxi) {
+            maxi = sum;
+            rowIndex = row;
+        }
+    }
+    cout<< "maximum sum is " << maxi <<endl;
+    cout<<endl;
+    return rowIndex;
+}
 int main() {
     //to create 2D array
-    int arr[3][4];
+    int arr[3][3];
     //to take input row wise
     cout<< "enter the elements"<<endl;
     for (int row=0; row<3; row++) {
-        for (int col=0; col<4; col++) {
+        for (int col=0; col<3; col++) {
             cin >> arr[row][col];
         }
     }
@@ -29,22 +59,31 @@ int main() {
     }
     */
     //to print
-    cout<< "printing the "<<endl;
+    cout<< "printing the array "<<endl;
     for (int row=0; row<3; row++) {
-        for (int col=0; col<4; col++) {
+        for (int col=0; col<3; col++) {
             cout<< arr[row][col] << " ";
         }
         cout<<endl;
     }
+    cout<<endl;
     //to find element
     cout<< "enter the element to search" <<endl;
     int target;
     cin >> target;
-    if (isPresent(arr, target, 3, 4)) {
+    if (isPresent(arr, target, 3, 3)) {
         cout<< "element found" <<endl;
     }
     else {
         cout<< "not found" <<endl;
     }
+    cout<<endl;
+    
+    printSum(arr, 3, 3);
+    cout<<endl;
+    
+    int ansIndex = largestrowSum(arr, 3, 3);
+    cout<< "max row is at index " << ansIndex <<endl;
+    cout<<endl;
     return 0;
 }
