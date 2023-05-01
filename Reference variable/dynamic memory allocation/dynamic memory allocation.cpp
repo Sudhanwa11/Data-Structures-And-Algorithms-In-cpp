18a43bd919a0f3cc275a2d2bea628bfe815b4b31
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-05-01 20:17:30 +0530

    Create dynamic memory allocation.cpp

---
+++ Reference variable/dynamic memory allocation/dynamic memory allocation.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int getSum (int *arr, int n) {
+    int sum = 0;
+    for (int i=0; i<n; i++) {
+        sum += arr[i];
+    }
+    return sum;
+}
+
+int main() {
+    
+    int n;
+    cout<< "enter number of elements"<<endl;
+    cin >> n;
+    
+    //variable size array
+    int *arr = new int [n];
+    
+    //taking input in array
+    cout<< "enter the elements inside"<<endl;
+    for (int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    
+    int ans = getSum(arr, n);
+    cout<< "answer is "<< ans <<endl;
+    
+    /*
+    char ch = 'q';
+    cout<< sizeof(ch) <<endl; //1 in heap
+    
+    //using pointer
+    char *c = &ch;
+    cout<< sizeof(c) <<endl; //8 in stack
+    */
+    return 0;
+}
