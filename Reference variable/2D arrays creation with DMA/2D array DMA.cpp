dcdf2dbdc18e26a245450bb19a8bbf6b52c18897
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-05-02 18:48:27 +0530

    Create 2D array DMA.cpp

---
+++ Reference variable/2D arrays creation with DMA/2D array DMA.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int row;
+    cin >> row;
+    
+    int col;
+    cin >> col;
+    
+    int **arr = new int *[row];
+    
+    //creating 2D array
+    for (int i=0; i<row; i++) {
+        arr[i] = new int [col];
+    }
+    
+    //taking input
+    for (int i=0; i<row; i++) {
+        for (int j=0; j<col; j++) {
+            cin >> arr[i][j];
+        }
+    }
+    cout<<endl;
+    
+    //printing output
+    for (int i=0; i<row; i++) {
+        for (int j=0; j<col; j++) {
+            cout << arr[i][j] << " ";
+        }cout<<endl;
+    }
+    
+    return 0;
+}
