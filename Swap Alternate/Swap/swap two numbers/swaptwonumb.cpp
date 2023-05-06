b20e9f2c773804a9a59bec677e2a37d12610ae03
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-05-06 15:59:05 +0530

    Update swaptwonumb.cpp

--- Swap Alternate/Swap/swap two numbers/swaptwonumb.cpp
+++ Swap Alternate/Swap/swap two numbers/swaptwonumb.cpp
@@ -1,8 +1,10 @@
 #include <bits/stdc++.h> 
 #include <utility>
 pair < int, int > swap(pair < int, int > swapValues) {
+    //to swap elements
     pair<int,int> temp;
     temp.first = swapValues.second;
     temp.second = swapValues.first;
+
     return temp;
 }
