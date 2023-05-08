2fbae01344d6785949064ab7f1d9c024f9ff6c4d
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-05-08 18:54:28 +0530

    Update primeornot.cpp

--- Easy codes/Prime or not/primeornot.cpp
+++ Easy codes/Prime or not/primeornot.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 using namespace std;
+
 bool isPrime (int n) {
+
     if (n<=1) {
         return false;
     }
+
     for (int i=2; i<n; i++) {
         if (n%i == 0) {
             return false;
@@ -11,15 +14,19 @@ bool isPrime (int n) {
     }
     return true;
 }
+
 int main() {
+
     int n;
     cout<< "enter the number"<<endl;
     cin>>n;
+
     if (isPrime(n)) {
         cout<< "is a prime number"<<endl;
     }
     else {
         cout<< "not a prime number"<<endl;
     }
+
     return 0;
 }
