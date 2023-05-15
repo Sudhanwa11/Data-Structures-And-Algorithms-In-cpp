e8acc111c04900b12bdeb88bf0f9680fd941cd14
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-05-15 12:02:21 +0530

    Update uniquenumberoccurence.cpp

--- ARRAYS CODES/Unique Number occurance/uniquenumberoccurence.cpp
+++ ARRAYS CODES/Unique Number occurance/uniquenumberoccurence.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+
     bool uniqueOccurrences(vector<int>& arr) {
+
         unordered_map<int, int> m;
         unordered_set<int> s;
         unordered_set<int> s1;
+
         for(int i = 0; i < arr.size(); i++){
             m[arr[i]]++;
         }
+
         for(int i = 0; i < arr.size(); i++){
             s1.insert(arr[i]);
         }
