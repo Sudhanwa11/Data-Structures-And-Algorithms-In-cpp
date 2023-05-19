23d835e177865c6c07bd6894bd2f3cdc95305d2f
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-05-19 14:12:39 +0530

    Update mergesortedarray.cpp

--- ARRAYS CODES/MERGE SORTED ARRAY/mergesortedarray.cpp
+++ ARRAYS CODES/MERGE SORTED ARRAY/mergesortedarray.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
+
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
 
         int i = m - 1;
         int j = n - 1;
         int k = n + m - 1;
+
 
         while (i >= 0 && j >= 0) {
             if (nums1[i] > nums2[j]) {
@@ -14,6 +16,7 @@ public:
                 nums1[k--] = nums2[j--];
             }
         }
+
 
         while (j >= 0) {
             nums1[k--] = nums2[j--];
