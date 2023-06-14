7a230b20344e645677565752e404ee24add6dcba
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-06-14 19:04:33 +0530

    Update MACRO.cpp

--- MACRO/MACRO/MACRO.cpp
+++ MACRO/MACRO/MACRO.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
+using namespace std;
 
+#define PI  3.14
+
+int main() {
+    
+    int r = 5;
+    int area = PI * r * r;
+    
+    cout << "area is " << area <<endl;
+    return 0;
+}
