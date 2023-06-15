67c155d5ba64a5a566df4a57e5c822c3eba95005
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-06-15 20:20:43 +0530

    Create powerof2.cpp

---
+++ RECURSION/Recursion/Power of 2/powerof2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int power (int n) {
+    //base case
+    if (n == 0) {
+        return 1;
+    }
+    //int followingnumber = power (n-1);
+    //int firstnumber = 2 * followingnumber;
+    return 2 * power (n-1);
+}
+int main() {
+    int n;
+    cin>>n;
+    
+    int ans = power(n);
+    cout << ans <<endl;
+
+    return 0;
+}
