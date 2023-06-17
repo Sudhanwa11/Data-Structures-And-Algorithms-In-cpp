e1daa40ad2a3dcb289b8e43bbdd1fe8230fcf911
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-06-17 16:56:45 +0530

    Update element check.cpp

--- RECURSION/Recursion/Element check/element check.cpp
+++ RECURSION/Recursion/Element check/element check.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
-
+void print (int arr[], int size) {
+    cout<< "size of array is-> " << size <<endl;
+    for (int i=0; i<size; i++) {
+        cout << arr[i] <<" ";
+    }
+    cout<<endl;
+}
 bool linearsearch (int arr[],int size,int key) {
+    print (arr,size);
     //base case
     if (size == 0) {
         return false;
@@ -14,11 +21,10 @@ bool linearsearch (int arr[],int size,int key) {
         return remainingpart;
     }
 }
-
 int main() {
     int arr[5] = {2, 4, 9, 10, 13};
     int size = 5;
-    int key = 3;
+    int key = 13;
     
     bool ans = linearsearch(arr, size, key);
     
