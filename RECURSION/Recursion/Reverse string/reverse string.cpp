49356e1d940c3d219fab8ab46ebba8ca891e82fb
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-06-19 19:01:32 +0530

    Create reverse string.cpp

---
+++ RECURSION/Recursion/Reverse string/reverse string.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void reverse (string &name, int i, int j) {
+    if(i>j) {
+        return;
+    }
+    swap(name[i],name[j]);
+    i++;
+    j--;
+    
+    reverse (name, i, j);
+}
+int main() {
+    string name = "bitch";
+    reverse(name, 0, name.length()-1);
+    
+    cout<< name <<endl;
+
+    return 0;
+}
