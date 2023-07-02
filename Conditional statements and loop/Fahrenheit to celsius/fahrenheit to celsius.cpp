f7c3546d3b2c4c370faebbdb82883e9e78e52c7b
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-07-02 17:04:51 +0530

    Create fahrenheit to celsius.cpp

---
+++ Conditional statements and loop/Fahrenheit to celsius/fahrenheit to celsius.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h> 
+#include <iostream>
+using namespace std;
+
+int main() {
+    int startf, endf, w;
+    int celsius;
+    cin>> startf>> endf>> w;
+
+    while (startf <= endf) {
+       celsius = (5.0/9)*(startf - 32) ;
+       cout << startf << " " << celsius << endl ;
+       startf = startf + w ;
+    }
+    return 0;
+}
