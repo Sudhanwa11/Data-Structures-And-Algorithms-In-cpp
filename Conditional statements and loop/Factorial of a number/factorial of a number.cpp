d69adcaf4042c96f35ba997ccc30ba32ae4ee8e1
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-07-02 18:07:18 +0530

    Create factorial of a number.cpp

---
+++ Conditional statements and loop/Factorial of a number/factorial of a number.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    int factor = 1;
+
+    if (n < 0) {
+      cout << "Error";
+    } 
+    else if (n == 0) {
+      cout << factor;
+    } 
+    else {
+      for (int i = 1; i <= n; i++)
+      factor = factor * i;
+      cout << factor;
+    }
+    return 0;
+}
