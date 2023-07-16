1fd65af9b363c81a067105deb85afda6ce2fe0dd
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-07-16 15:44:21 +0530

    Update sum of infinite array.cpp

--- CODE STUDIO /Sum of infinite arrays/sum of infinite array.cpp
+++ CODE STUDIO /Sum of infinite arrays/sum of infinite array.cpp
@@ -10,17 +10,17 @@ vector<int> sumInRanges(vector<int> &arr, int n, vector<vector<long long>> &quer
     vector<long long> prefix_sum(n+1, 0);
     vector<int> result;
     for(int i=1; i<=n; i++){
-        prefix_sum[i] =((arr[i-1] + prefix_sum[i-1])%mod);
+        prefix_sum[i] = ((arr[i-1] + prefix_sum[i-1])%mod);
     }
     for(int j=0; j<q; j++){
-        long long l=queries[j][0];
-        long long r=queries[j][1];
+        long long l = queries[j][0];
+        long long r = queries[j][1];
 
         long long left_sum = sum_upto_i(prefix_sum, l-1, n);
 
         long long right_sum = sum_upto_i(prefix_sum, r, n);
         
-        result.push_back((right_sum-left_sum+mod)%mod);
+        result.push_back((right_sum-left_sum+mod) % mod);
     }
     return result;
 }
