d562904504efc5fcadfd7c04cd2ba3a68bf0f660
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-07-17 17:27:07 +0530

    Create count all subarrays having sum divisible by k.cpp

---
+++ CODE STUDIO /Count all subarrays having sum divisible by k/count all subarrays having sum divisible by k.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h> 
+int subArrayCount(vector<int> &arr, int k) {
+
+    unordered_map <int, int> reMap;
+    reMap[0] = 1;
+    long sum = 0;
+    int count = 0;
+    
+    for (int i=0; i<arr.size(); i++) {
+        sum += arr[i];
+        int rem = (sum % k + k) % k;
+        if (reMap.find(rem) != reMap.end()) {
+            count += reMap[rem];
+            reMap[rem] += 1; 
+        }
+        else {
+            reMap[rem] = 1;
+        }
+    }
+    return count;
+}
