cf2f9892c89c5e41b1916a1f6e4457788485cea4
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-07-25 14:08:26 +0530

    Update XOR Query.cpp

--- CODE STUDIO /XOR Query/XOR Query.cpp
+++ CODE STUDIO /XOR Query/XOR Query.cpp
@@ -1,17 +1,16 @@
 #include <bits/stdc++.h> 
 vector<int> xorQuery(vector<vector<int>> &queries)
 {
-	vector<int> ans;
-
+    vector<int> ans;
     for (int i = 0; i < queries.size(); i++) {
-		if (queries[i][0] == 1) {
-			ans.push_back(queries[i][1]);
+	   if (queries[i][0] == 1) {
+		ans.push_back(queries[i][1]);
         }
-		else {
-			for (int j = 0; j < ans.size(); j++) {
-				ans[j] = ans[j] ^ queries[i][1];
+	   else {
+		for (int j = 0; j < ans.size(); j++) {
+			ans[j] = ans[j] ^ queries[i][1];
             }
         }
     }
-	return ans;
+    return ans;
 }
