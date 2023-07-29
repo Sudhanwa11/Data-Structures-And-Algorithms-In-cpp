fb6dc9bc8843134305daf01d2bd369a8517c947a
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-07-29 16:30:07 +0530

    Create decode the message.cpp

---
+++ CODE STUDIO /Decode the message/decode the message.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h> 
+string encode(string &message) {
+    int i=0, j = i+1;
+    string encoded = "";
+
+    while (i < message.size()) {
+        if (message[i] == message[j]) {
+            j++;
+        }
+        else {
+            int len = j-i;
+            encoded += message[i] + to_string(len);
+            i = j;
+        }
+    }
+    return encoded;
+}
