bbeb4bbe8b35d52fe6204fb293f1680a96c2913f
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-08-12 16:05:42 +0530

    Update subset.cpp

--- RECURSION/Recursion/SUBSET/subset.cpp
+++ RECURSION/Recursion/SUBSET/subset.cpp
@@ -1,6 +1,7 @@
 class Solution {
 private: 
     void solve (vector<int> nums, vector <int> output, int index, vector<vector<int>>& ans) {
+        
         //base case
         if (index >= nums.size()) {
             ans.push_back(output);
@@ -16,6 +17,7 @@ private:
     }
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
+        
         vector<vector<int>> ans;
         vector <int> output;
         int index = 0;
