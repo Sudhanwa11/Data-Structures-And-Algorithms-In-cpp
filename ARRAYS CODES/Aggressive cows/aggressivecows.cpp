aff5c991b1a71401994d5192e0f53ad457a553bc
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-08-18 14:53:28 +0530

    Update aggressivecows.cpp

--- ARRAYS CODES/Aggressive cows/aggressivecows.cpp
+++ ARRAYS CODES/Aggressive cows/aggressivecows.cpp
@@ -12,7 +12,6 @@ bool isPossible(vector<int> &stalls, int k, int mid) {
     }
     return false;
 }
-
 int aggressiveCows(vector<int> &stalls, int k) {
     //to sort array first
     sort(stalls.begin(), stalls.end());
