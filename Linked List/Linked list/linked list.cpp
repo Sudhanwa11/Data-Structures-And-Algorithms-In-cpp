1ccd710c790d7ead93ddf31f5b0805c36eb4751a
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-09-01 20:19:39 +0530

    Create linked list.cpp

---
+++ Linked List/Linked list/linked list.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class node {
+    public:
+    int data;
+    node* next;
+    
+    node (int data) {
+        this -> data = data;
+        this -> next = NULL;
+    }
+};
+
+void insertatHead (node* &head, int value) {
+    node* temp = new node(value);
+    temp -> next = head;
+    head = temp;
+}
+
+void insertatTail (node* &tail, int value) {
+    node* temp = new node(value);
+    tail -> next = temp;
+    tail = temp;
+}
+
+void printll (node* &head) {
+    node* temp = head;
+    while (temp != NULL) {
+        cout<< temp -> data <<" ";
+        temp = temp -> next;
+    }
+    cout<<endl;
+}
+
+int main() {
+    node* node1 = new node(15);
+    cout<< node1 -> data <<endl;
+    cout<< node1 -> next <<endl;
+    
+    node* head = node1;
+    node* tail = node1;
+    
+    cout<< "inserting 10 at head" <<endl;
+    insertatHead (head, 10);
+    printll (head);
+    
+    cout<< "inserting 20 at tail" <<endl;
+    insertatTail (tail, 20);
+    printll (head);
+    
+    return 0;
+}
