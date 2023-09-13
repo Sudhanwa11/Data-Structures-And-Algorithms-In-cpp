a0eace9395fa02c9d60fef34e3d98ad623777fb1
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-09-13 17:22:54 +0530

    Create circularly linked.cpp

---
+++ Linked List/Circularly Linked/circularly linked.cpp
@@ -0,0 +1,21 @@
+bool isCircular(Node* head) {
+    if (head == NULL) {
+        return true;
+    }
+
+    if (head -> next == NULL ) {
+        return false;
+    }
+
+    Node* temp = head -> next;
+
+    while (temp != NULL) {
+        if (temp -> data == head -> data) {
+            return true;
+        }
+        else if (temp -> data == NULL) return false;
+        temp -> data = NULL;
+        temp = temp -> next;
+    }
+    return false;
+}
