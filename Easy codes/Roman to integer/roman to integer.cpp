ec923af6e03a392e052cda0e6c502e4d20f0fadc
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-09-14 15:21:49 +0530

    Update roman to integer.cpp

--- Easy codes/Roman to integer/roman to integer.cpp
+++ Easy codes/Roman to integer/roman to integer.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int romanToInt(string s) {
+        //Create a map to store the values of Roman numerals
         unordered_map<char, int> romanValues = {
             {'I', 1},
             {'V', 5},
@@ -11,20 +12,27 @@ public:
             {'M', 1000}
         };
 
+        //Initialize variables to store the result and previous character's value
         int result = 0;
         int prevValue = 0;
 
-        for (int i=0; i<=s.length()-1; i++) {
+        //iterate over the string from left to right
+        for (int i=0; i<s.length(); i++) {
+            //Get the value of the current character
             int currValue = romanValues[s[i]];
+
+            // If the current value is greater than the previous value, subtract the previous value
             if (currValue > prevValue) {
                 result += currValue - 2 * prevValue;
             } 
+            // Otherwise, add the current value to the result
             else {
                 result += currValue;
             }
+
+            // Update the previous value for the next iteration
             prevValue = currValue;
         }
-
         return result;
     }
 };
