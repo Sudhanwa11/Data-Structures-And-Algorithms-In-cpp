1fa1db8981e35450615a0375fbc088025bac8b9c
Sudhanwa Vaid <127226833+Sudhanwa11@users.noreply.github.com>
2023-09-17 17:41:51 +0530

    Update ROTATEARRAY.cpp

--- ARRAYS CODES/ROTATE ARRAY/ROTATEARRAY.cpp
+++ ARRAYS CODES/ROTATE ARRAY/ROTATEARRAY.cpp
@@ -1,11 +1,19 @@
 class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
+    // Create a temporary vector to hold the rotated elements
+    vector<int> temp(nums.size());
+
+    // Iterate through each element in the original vector
+    for (int i = 0; i < nums.size(); i++) {
+        // Calculate the new index after rotation
+        int new_index = (i + k) % nums.size();
         
-        vector<int> temp(nums.size());
-        for (int i=0; i<nums.size(); i++) {
-            temp[(i+k) % nums.size()] = nums[i];
-        }
-        nums = temp;
+        // Place the current element at the new index in the temporary vector
+        temp[new_index] = nums[i];
+    }
+
+    // Update the original vector with the rotated values
+    nums = temp;
     }
 };
